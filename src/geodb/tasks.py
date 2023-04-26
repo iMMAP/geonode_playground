75e438a575b7fbddefc6d83591fd6d323190653d
obediobadiah <obediobadiah05@gmail.com>
2023-04-26 13:02:36 +0200

    geodb background tasks

--- src/geodb/tasks.py
+++ src/geodb/tasks.py
@@ -1,20 +1,18 @@
-#stdlib imports
-import urllib2
-import urllib
-import urllib3
-import json
-import os.path
-from datetime import datetime,timedelta
-import re
-from xml.dom import minidom 
-import sys
-import shutil
-from collections import OrderedDict
-import calendar
 
-#third-party imports
-from neicmap import distance
-from libcomcat import fixed
-import numpy
-import types
+from celery.task.schedules import crontab
+from celery.decorators import periodic_task, task
+from celery.utils.log import get_task_logger
+from datetime import datetime
+from geodb.views import getForecastedDisaster, updateSummaryTable, getSnowCover, getLatestEarthQuake, getLatestShakemap, databasevacumm, runGlofasDownloader
+from geodb.zonal_stats import downloadtif
+from dashboard.views import classmarkerGet
 
+logger = get_task_logger(__name__)
+
+@periodic_task(run_every=(crontab(hour='*')))
+def updateLatestEarthQuake():
+	getLatestEarthQuake()
+
+@periodic_task(run_every=(crontab(hour='*')))
+def updateLatestShakemap():
+	getLatestShakemap(True)
