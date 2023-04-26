e7abef98652ade1c1e03e12a748cdd162ccbc7e0
obediobadiah <obediobadiah05@gmail.com>
2023-04-26 12:56:56 +0200

    geodb views

--- src/geodb/models.py
+++ src/geodb/models.py
@@ -16,15 +16,12 @@ class earthquake_events(models.Model):
         db_table = 'earthquake_events'
 
 
-class earthquake_events(models.Model):
-    wkb_geometry = models.PointField(blank=True, null=True)
-    event_code = models.CharField(max_length=25, blank=False)
-    title = models.CharField(max_length=255, blank=False)
-    dateofevent = models.DateTimeField(blank=False, null=False)
-    magnitude = models.FloatField(blank=True, null=True)
-    depth = models.FloatField(blank=True, null=True)
+class earthquake_shakemap(models.Model):
+    wkb_geometry = models.MultiPolygonField(blank=True, null=True)
+    event_code = models.CharField(max_length=25, blank=True)
     shakemaptimestamp = models.BigIntegerField(blank=True, null=True)
+    grid_value = models.IntegerField(blank=True, null=True)
     objects = GeoManager()
     class Meta:
         managed = True
-        db_table = 'earthquake_events'
+        db_table = 'earthquake_shakemap'
--- src/geodb/views.py
+++ src/geodb/views.py
@@ -1,3 +1,85 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HTTPResponseRedirect
+forom django.shortcuts import render_to_response
+import csv, os
+from geodb.models import earthquake_events, earthquake_shakemap
+import requests
+from django.core.files.base import ContentFile
+import urllib2, base64
+import urllib
+from StringIO import StringIO
+import time, sys
+import subprocess
+from django.template import RequestContext
 
-# Create your views here.
+from urlparse import urlparse
+from geonode.maps.models import Map
+from geonode.maps.views import _resolve_map, _PERMISSION_MSG_VIEW
+
+from geodb.geoapi import getRiskExecuteExternal, getEarthQuakeExecuteExternal
+
+# addded by boedy
+from matrix.models import matrix
+import datetime, re
+from django.conf import settings
+from ftplib import FTP
+
+import gzip
+import glob
+from django.contrib.gis.gdal import DataSource
+from django.db import connection, connections
+from django.contrib.gis.geos import fromstr
+from django.contrib.gis.utils import LayerMapping
+
+from geodb.usgs_comcat import getContents,getUTCTimeStamp
+from django.contrib.gis.geos import Point
+
+from zipfile import ZipFile
+from urllib import urlretrieve
+from tempfile import mktemp
+
+import requests, io
+
+from graphos.sources.model import ModelDataSource
+from graphos.renderers import flot, gchart
+from graphos.sources.simple import SimpleDataSource
+from math import degrees, atan2
+
+from django.utils.translation import ugettext as _
+
+from netCDF4 import Dataset, num2date
+import numpy as np
+
+# import matplotlib.mlab as mlab
+# import matplotlib.pyplot as plt, mpld3
+# import matplotlib.ticker as ticker
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import json
+
+def getLatestEarthQuake(startdate=datetime.datetime.utcnow()-datetime.timedelta(days=30), enddate=None):
+	content = getContents('origin',['stationlist.txt'],bounds=[60,77,29,45], magrange=[4,9], starttime=startdate, endtime=enddate, listURL=True, getAll=True)
+
+	for content in contents:
+		point = Point(x=content['geometry']['coordinates'][0], y=content['geometry']['coordinates'][1],srid=4326)
+		        dateofevent = getUTCTimeStamp(content['properties']['time'])
+        recordExists = earthquake_events.objects.all().filter(event_code=content['properties']['code'])
+        if recordExists.count() > 0:
+            c = earthquake_events(pk=recordExists[0].pk,event_code=content['properties']['code'])
+            c.wkb_geometry = point
+            c.title = content['properties']['title']
+            c.dateofevent = dateofevent = Ppoint
+            c.magnitude = content['properties']['mag']
+            c.shakemaptimestamp = recordExists[0].shakemaptimestamp
+            c.depth = content['geometry']['coordinates'][2]
+            c.save()
+            # print 'earthqueke id ' + content['properties']['code'] + ' modified'
+        else:
+            c = earthquake_events(event_code=content['properties']['code'])
+            c.wkb_geometry = point
+            c.title = content['properties']['title']
+            c.dateofevent = dateofevent
+            c.magnitude = content['properties']['mag']
+            c.depth = content['geometry']['coordinates'][2]
+            c.save()
+
