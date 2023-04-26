3434e08e5f2cb82ce26e0c9c2b9e89832126fc88
obediobadiah <obediobadiah05@gmail.com>
2023-04-26 12:22:11 +0200

    geodb calculation models

--- src/geodb/models.py
+++ src/geodb/models.py
@@ -1,3 +1,30 @@
-from django.db import models
+from __future__ import unicode_literals
+from django.contrib.gis.db import models
+from django.db.models import Manager as GeoManager
 
-# Create your models here.
+class earthquake_events(models.Model):
+    wkb_geometry = models.PointField(blank=True, null=True)
+    event_code = models.CharField(max_length=25, blank=False)
+    title = models.CharField(max_length=255, blank=False)
+    dateofevent = models.DateTimeField(blank=False, null=False)
+    magnitude = models.FloatField(blank=True, null=True)
+    depth = models.FloatField(blank=True, null=True)
+    shakemaptimestamp = models.BigIntegerField(blank=True, null=True)
+    objects = GeoManager()
+    class Meta:
+        managed = True
+        db_table = 'earthquake_events'
+
+
+class earthquake_events(models.Model):
+    wkb_geometry = models.PointField(blank=True, null=True)
+    event_code = models.CharField(max_length=25, blank=False)
+    title = models.CharField(max_length=255, blank=False)
+    dateofevent = models.DateTimeField(blank=False, null=False)
+    magnitude = models.FloatField(blank=True, null=True)
+    depth = models.FloatField(blank=True, null=True)
+    shakemaptimestamp = models.BigIntegerField(blank=True, null=True)
+    objects = GeoManager()
+    class Meta:
+        managed = True
+        db_table = 'earthquake_events'
