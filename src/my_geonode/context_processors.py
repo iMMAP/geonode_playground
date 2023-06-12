fce7e1bf341abc15a7b3462d879365664569b963
Obedi Obadiah <60090007+obediobadiah@users.noreply.github.com>
2023-06-12 13:50:22 +0200

    google analystic

--- src/my_geonode/context_processors.py
+++ src/my_geonode/context_processors.py
@@ -1,6 +1,6 @@
-import os 
+# import os 
 
-def export_vars(request):
-    data = {}
-    data['APP_ENV'] = os.environ['APP_ENV']
-    return data
+# def export_vars(request):
+#     data = {}
+#     data['APP_ENV'] = os.environ['APP_ENV']
+#     return data
