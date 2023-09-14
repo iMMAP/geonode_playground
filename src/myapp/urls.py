77113cbdbad8277fab78d7b438a5b1001cab12a2
Shahryar Tayeb <shahryar.dev@gmail.com>
2023-09-14 14:44:20 +0430

    new itt report page

--- src/myapp/urls.py
+++ src/myapp/urls.py
@@ -4,6 +4,7 @@ from geonode.urls import path, urlpatterns
 from . import views
 
 urlpatterns = [
+    path('itt_stats',views.itt_stats,name="itt_stats"),
     path('landing',TemplateView.as_view(template_name='myapp/landing.html'),name='landing'),
     path('ocha_dashboards',views.ocha_dashboards,name='ocha_dashboards'),
     path('ocha_dashboards/<str:slug>',views.ocha_dashboard,name='ocha_dashboards.show'),
--- src/myapp/views.py
+++ src/myapp/views.py
@@ -2,9 +2,55 @@ from allauth.account.views import SignupView
 from django.conf import settings
 from django.db.models import Q
 from django.shortcuts import render
-
+from geonode.people.models import Profile
 from .models import OchaDashboard
+from .decorators import staff_or_404
+from django.utils import timezone
+from django.db.models import Count, Case, When, IntegerField, Sum
 
+
+
+
+@staff_or_404
+def itt_stats(request):
+    active_users = Profile.objects.filter(is_active=True).count()
+    staff_count = Profile.objects.filter(is_staff=True).count()
+    
+    now = timezone.now()
+    month_start = timezone.datetime(now.year, now.month, 1, tzinfo=timezone.utc)
+    active_users_current_month = Profile.objects.filter(last_login__gte=month_start, is_active=True).count()
+    
+    users_per_position = Profile.objects.values('position').annotate(user_count=Count('id'))
+    users_per_country = Profile.objects.values('country').annotate(user_count=Count('id'))
+    
+    users_per_org = Profile.objects.values('organization').annotate(
+        user_count=Count('id'),
+        active_user_count=Sum(
+            Case(
+                When(is_active=True, then=1),
+                default=0,
+                output_field=IntegerField()
+            )
+        ),
+        inactive_user_count=Sum(
+            Case(
+                When(is_active=False, then=1),
+                default=0,
+                output_field=IntegerField()
+            )
+        )
+    )
+
+    context = {
+        "active_users": active_users,
+        "staff_count": staff_count,
+        "active_users_current_month": active_users_current_month,
+        "users_per_position":users_per_position,
+        "users_per_country":users_per_country,
+        "users_per_org":users_per_org
+    }
+    
+    return render(request, 'myapp/itt_stats.html', context) 
 
 def ocha_dashboards(request):
     q = request.GET.get('q')
