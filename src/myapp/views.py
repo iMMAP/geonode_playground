c1299fa794368588273c3afd6a49410e0d484831
Shahryar Tayeb <shahryar.dev@gmail.com>
2023-09-19 09:28:34 +0430

    added date filter for itt

--- src/myapp/views.py
+++ src/myapp/views.py
@@ -9,21 +9,51 @@ from django.utils import timezone
 from django.db.models import Count, Case, When, IntegerField, Sum
 
 
+from django.http import Http404
+from calendar import monthrange
+from datetime import datetime
+
 
 
 @staff_or_404
 def itt_stats(request):
-    active_users = Profile.objects.filter(is_active=True).count()
-    staff_count = Profile.objects.filter(is_staff=True).count()
-    
+    month_param = request.GET.get('month')
     now = timezone.now()
-    month_start = timezone.datetime(now.year, now.month, 1, tzinfo=timezone.utc)
-    active_users_current_month = Profile.objects.filter(last_login__gte=month_start, is_active=True).count()
     
-    users_per_position = Profile.objects.values('position').annotate(user_count=Count('id'))
-    users_per_country = Profile.objects.values('country').annotate(user_count=Count('id'))
-    
-    users_per_org = Profile.objects.values('organization').annotate(
+    # Generate month links
+    current_month = now.month
+    months = [
+        {
+            'number': i,
+            'name': datetime(year=2022, month=i, day=1).strftime('%B'),  # Use any year, we just need the month name
+            'is_future': i > current_month
+        }
+        for i in range(1, 13)
+    ]
+
+    if month_param is None:
+        month = now.month
+    else:
+        try:
+            month = int(month_param)
+        except ValueError:
+            raise Http404("Invalid month parameter")
+
+        if month > 12 or month < 1 or month > now.month:
+            raise Http404("Invalid month parameter")
+
+    _, last_day = monthrange(now.year, month)
+    month_start = timezone.datetime(now.year, month, 1, tzinfo=timezone.utc)
+    month_end = timezone.datetime(now.year, month, last_day, tzinfo=timezone.utc)
+
+    active_users = Profile.objects.filter(is_active=True, last_login__range=(month_start, month_end)).count()
+    staff_count = Profile.objects.filter(is_staff=True, last_login__range=(month_start, month_end)).count()
+    active_users_current_month = Profile.objects.filter(last_login__range=(month_start, month_end), is_active=True).count()
+
+    users_per_position = Profile.objects.filter(last_login__range=(month_start, month_end)).values('position').annotate(user_count=Count('id'))
+    users_per_country = Profile.objects.filter(last_login__range=(month_start, month_end)).values('country').annotate(user_count=Count('id'))
+
+    users_per_org = Profile.objects.filter(last_login__range=(month_start, month_end)).values('organization').annotate(
         user_count=Count('id'),
         active_user_count=Sum(
             Case(
@@ -47,10 +77,69 @@ def itt_stats(request):
         "active_users_current_month": active_users_current_month,
         "users_per_position":users_per_position,
         "users_per_country":users_per_country,
-        "users_per_org":users_per_org
+        "users_per_org":users_per_org,
+        "months": months,
     }
+
+    return render(request, 'myapp/itt_stats.html', context)
+
+
+# @staff_or_404
+# def itt_stats(request):
+#     month_param = request.GET.get('month')
+#     now = timezone.now()
+
+#     if month_param is None:
+#         month = now.month
+#     else:
+#         try:
+#             month = int(month_param)
+#         except ValueError:
+#             raise Http404("Invalid month parameter")
+
+#         if month > 12 or month < 1 or month > now.month:
+#             raise Http404("Invalid month parameter")
+
+#     month_start = timezone.datetime(now.year, month, 1, tzinfo=timezone.utc)
     
-    return render(request, 'myapp/itt_stats.html', context) 
+#     print(month_start)
+    
+#     active_users = Profile.objects.filter(is_active=True, last_login__gte=month_start).count()
+#     staff_count = Profile.objects.filter(is_staff=True, last_login__gte=month_start).count()
+#     active_users_current_month = Profile.objects.filter(last_login__gte=month_start, is_active=True).count()
+    
+#     users_per_position = Profile.objects.filter(last_login__gte=month_start).values('position').annotate(user_count=Count('id'))
+#     users_per_country = Profile.objects.filter(last_login__gte=month_start).values('country').annotate(user_count=Count('id'))
+    
+#     users_per_org = Profile.objects.filter(last_login__gte=month_start).values('organization').annotate(
+#         user_count=Count('id'),
+#         active_user_count=Sum(
+#             Case(
+#                 When(is_active=True, then=1),
+#                 default=0,
+#                 output_field=IntegerField()
+#             )
+#         ),
+#         inactive_user_count=Sum(
+#             Case(
+#                 When(is_active=False, then=1),
+#                 default=0,
+#                 output_field=IntegerField()
+#             )
+#         )
+#     )
+
+#     context = {
+#         "active_users": active_users,
+#         "staff_count": staff_count,
+#         "active_users_current_month": active_users_current_month,
+#         "users_per_position":users_per_position,
+#         "users_per_country":users_per_country,
+#         "users_per_org":users_per_org
+#     }
+    
+#     return render(request, 'myapp/itt_stats.html', context) 
+
 
 def ocha_dashboards(request):
     q = request.GET.get('q')
