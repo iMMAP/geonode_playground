c6714f33765c691ff6089ca9358099570241e4e5
Obedi Obadiah <60090007+obediobadiah@users.noreply.github.com>
2023-10-02 15:20:53 +0200

    Production earthquake analysis

--- src/geodb/tasks.py
+++ src/geodb/tasks.py
@@ -2,6 +2,7 @@
 from celery import shared_task
 from celery.utils.log import get_task_logger
 from geodb.views import getLatestEarthQuake, getLatestShakemap
+from geodb.EarthquakeHistoricalAnalysis import getEarthquakeHistoricalAnalysis
 
 logger = get_task_logger(__name__)
 
@@ -12,3 +13,7 @@ def updateLatestEarthQuake():
 @shared_task
 def updateLatestShakemap():
 	getLatestShakemap()
+
+@shared_task
+def getEarthquakeHistorical:
+	getEarthquakeHistoricalAnalysis()
