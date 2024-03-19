6ba9fe80c06242e4690740a7440de28bdb8d19ca
Obedi Obadiah <60090007+obediobadiah@users.noreply.github.com>
2024-03-19 09:09:07 +0200

    production url

--- src/myapp/views.py
+++ src/myapp/views.py
@@ -132,8 +132,8 @@ def landing_redirect(request):
         return render(request, 'myapp/landing.html')
 
     elif next_url and next_url.startswith('/'):
-        login_url = f"https://dev.hsdc.immap.org/account/login/?next={next_url}"
-        # login_url = f"https://hsdc.immap.org/account/login/?next={next_url}"
+        # login_url = f"https://dev.hsdc.immap.org/account/login/?next={next_url}"
+        login_url = f"https://hsdc.immap.org/account/login/?next={next_url}"
         return redirect(login_url)
 
     else:
