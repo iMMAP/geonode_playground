e7c9e6fcb2612df5ae61753411218ddbabaf7c72
Obedi Obadiah <60090007+obediobadiah@users.noreply.github.com>
2024-09-20 13:31:17 +0200

    itt report link

--- src/my_geonode/templatetags/get_menu_json.py
+++ src/my_geonode/templatetags/get_menu_json.py
@@ -242,6 +242,12 @@ def get_user_menu(context):
             "label": "Help"
         },
         devider,
+        {
+            "type": "link",
+            "href": "/itt_stats",
+            "label": "ITT Stats Report"
+        },
+        devider,
         logout
     ]
     monitoring = []
