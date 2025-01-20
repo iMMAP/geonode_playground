f8c190ebe813ccaab7ccd8d60791ba65f1a8758d
Obedi Obadiah <60090007+obediobadiah@users.noreply.github.com>
2025-01-20 23:31:28 +0200

    registration improvements

--- src/myapp/forms.py
+++ src/myapp/forms.py
@@ -8,6 +8,8 @@ class SimpleSignupForm(SignupForm):
     first_name = forms.CharField(max_length=50, label='first_name')
     last_name = forms.CharField(max_length=50, label='last_name')
     profile = forms.CharField(max_length=50, label='profile')
+    city = forms.CharField(max_length=50, label='city')
+    position = forms.CharField(max_length=50, label='position')
 
     def save(self, request):
         user = super(SimpleSignupForm, self).save(request)
@@ -15,5 +17,7 @@ class SimpleSignupForm(SignupForm):
         user.first_name = self.cleaned_data['first_name']
         user.last_name = self.cleaned_data['last_name']
         user.profile = self.cleaned_data['profile']
+        user.city = self.cleaned_data['city']
+        user.position = self.cleaned_data['position']
         user.save()
         return user
